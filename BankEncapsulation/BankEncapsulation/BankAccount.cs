c4a180f6cee73cdc0929b12a279fa77bb216ec9f
Jake Merlin <JacobMerlin92@outlook.com>
2022-07-17 17:22:33 -0400

    first push

---
+++ BankEncapsulation/BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public static double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance += amount;
+        }
+        public static double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation/BankEncapsulation/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            double amount = 100;
+            BankAccount.balance = amount;
+
+            Console.WriteLine(BankAccount.GetBalance());
+        }
+    }
+}
